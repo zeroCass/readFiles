57acb6650aab15053394c28bdd77ca030a1bfd9c
Mateus Valério <mateusvalerio1@gmail.com>
2021-09-08 11:14:43 -0300

    executeCommand class. This class has a method that instanciate a new Process builder each time that is called. Then recieve the command to execute. So i call it in a loop for each file

--- src/readfiles/ReadFiles.java
+++ src/readfiles/ReadFiles.java
@@ -13,6 +13,41 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReadFiles {
+    
+    
+    public static String executeCommand (String command) {
+        String line;
+        String resultat = "";
+        try {
+            ProcessBuilder builder;
+
+            builder = new ProcessBuilder("cmd.exe", "/c", command);
+    
+            
+            builder.redirectErrorStream(true);
+            Process p = builder.start();
+            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while (true) {
+                line = r.readLine();
+                if (line == null) {
+                    break;
+                }
+                resultat += line + "\n";
+            }
+        } catch (IOException e) {
+            System.out.println("Exception = " + e.getMessage());
+        }
+        return resultat;
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     public static void main(String[] args) throws IOException, InterruptedException {
         
        //Recebe o diretorio dos arquivos
@@ -21,7 +56,10 @@ public class ReadFiles {
         //File newFile = new File("C:\\Users\\mayll\\OneDrive\\Documentos\\Lab1_OAC\\videoFiles\\frames\\S\\bigS.s");
         //toFileS toS = new toFileS(dir,newFile);
         
-    ProcessBuilder processBuilder = new ProcessBuilder("C:/Windows/System32/cmd.exe");
+        
+        
+//    ProcessBuilder processBuilder = new ProcessBuilder("C:/Windows/System32/cmd.exe");
+//    processBuilder.directory(new File("C:\\Users\\mateu\\OneDrive\\Documentos\\OAC\\Lab 1\\Video Files\\frames"));
 
     // -- Linux --
 
@@ -35,28 +73,33 @@ public class ReadFiles {
 
     // Run a command
     
-    Process process = processBuilder.start();
-    BufferedWriter p_stdin = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
-    File dir = new File("C:\\Users\\mayll\\OneDrive\\Documentos\\Lab1_OAC\\videoFiles\\frames\\teste");
+//    Process process = processBuilder.start();
+//    BufferedWriter p_stdin = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
+    File dir = new File("C:\\Users\\mateu\\OneDrive\\Documentos\\OAC\\Lab 1\\Video Files\\frames");
     for (File file : dir.listFiles()) {
-        
+
         if(file.getName().contains(".bmp")) {
             String fileName = file.getName().replace(".bmp", "");
             System.out.println(fileName);
-        
-            try {
-                //single execution
-                p_stdin.write("cd C:\\Users\\mayll\\OneDrive\\Documentos\\Lab1_OAC\\videoFiles\\frames\\teste");
-                p_stdin.newLine();
-                p_stdin.write("bmp2oac3.exe " + fileName + ".bmp");
-                p_stdin.newLine();
-                p_stdin.write("del /f " + fileName + ".mif");
-                p_stdin.newLine();
-                p_stdin.flush();
-            }
-            catch (IOException e) {
-                System.out.println(e);
-            }
+            String result = executeCommand("cd C:\\Users\\mateu\\OneDrive\\Documentos\\OAC\\Lab 1\\Video Files\\frames && bmp2oac3.exe "+ fileName + ".bmp");
+            executeCommand("cd C:\\Users\\mateu\\OneDrive\\Documentos\\OAC\\Lab 1\\Video Files\\frames && del /f " + fileName + ".mif");
+            executeCommand("cd C:\\Users\\mateu\\OneDrive\\Documentos\\OAC\\Lab 1\\Video Files\\frames && del /f " + fileName + ".data");
+            System.out.println(result);
+            
+            
+//            try {
+//                //single execution
+//                p_stdin.write("cd C:\\Users\\mayll\\OneDrive\\Documentos\\Lab1_OAC\\videoFiles\\frames\\teste");
+//                p_stdin.newLine();
+//                p_stdin.write("bmp2oac3.exe " + fileName + ".bmp");
+//                p_stdin.newLine();
+//                p_stdin.write("del /f " + fileName + ".mif");
+//                p_stdin.newLine();
+//                p_stdin.flush();
+//            }
+//            catch (IOException e) {
+//                System.out.println(e);
+//            }
         
         }
         
@@ -64,54 +107,18 @@ public class ReadFiles {
         
         //processBuilder.command("cmd.exe", "/c", "cd C:\\Users\\mayll\\OneDrive\\Documentos\\Lab1_OAC\\videoFiles\\frames\\imagens && bmp2oac3.exe " + fileName);
     }
-    Scanner s = new Scanner( process.getInputStream() );
-    while (s.hasNext())
-    {
-        System.out.println( s.next() );
-    }
-       s.close();
-    p_stdin.write("exit");
-    p_stdin.newLine();
-    p_stdin.flush();
-    System.exit(0);
-    // Run a bat file
-    //processBuilder.command("C:\\Users\\mkyong\\hello.bat");
+//    Scanner s = new Scanner( process.getInputStream() );
+//    while (s.hasNext())
+//    {
+//        System.out.println( s.next() );
+//    }
+//       s.close();
+//    p_stdin.write("exit");
+//    p_stdin.newLine();
+//    p_stdin.flush();
+//    int exit = process.waitFor();
+//    System.out.println("Exit status: " + exit);
 
-  /*  try {
-
-        Process process = processBuilder.start();
-
-        StringBuilder output = new StringBuilder();
-
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(process.getInputStream()));
-
-        String line;
-        while ((line = reader.readLine()) != null) {
-            output.append(line + "\n");
-        }
-
-        int exitVal = process.waitFor();
-        if (exitVal == 0) {
-            System.out.println("Success!");
-            System.out.println(output);
-            //System.exit(0);
-        } else {
-            //abnormal...
-        }
-
-    } catch (IOException e) {
-        e.printStackTrace();
-    } catch (InterruptedException e) {
-        e.printStackTrace();
-    }*/
-    
-    
-
-        //ProcessBuilder builder = new ProcessBuilder();
-        //builder.command("cmd.exe", "/c", "dir");
-        //builder.directory(new File("C:\\Users\\mayll\\OneDrive\\Documentos\\Lab1_OAC\\videoFiles\\frames\\imagens"));
-        //builder.start();
         
 
         
