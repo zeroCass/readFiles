9c557b6eeca7bf33374879c4af31dac5ae36b17e
mayMoones <mayllak@hotmail.com>
2021-09-08 20:13:18 -0300

    One File .bin
    -Função que lê e escreve os arquivos. .bin em apenas um só arquivo

--- src/readfiles/ReadFiles.java
+++ src/readfiles/ReadFiles.java
@@ -1,4 +1,3 @@
-
 package readfiles;
 
 import java.io.BufferedReader;
@@ -13,116 +12,16 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReadFiles {
-    
-    
-    public static String executeCommand (String command) {
-        String line;
-        String resultat = "";
-        try {
-            ProcessBuilder builder;
-
-            builder = new ProcessBuilder("cmd.exe", "/c", command);
-    
-            
-            builder.redirectErrorStream(true);
-            Process p = builder.start();
-            BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            while (true) {
-                line = r.readLine();
-                if (line == null) {
-                    break;
-                }
-                resultat += line + "\n";
-            }
-        } catch (IOException e) {
-            System.out.println("Exception = " + e.getMessage());
-        }
-        return resultat;
-    
-    }
-    
-    
-    
-    
-    
-    
-    
-    
     public static void main(String[] args) throws IOException, InterruptedException {
         
-       //Recebe o diretorio dos arquivos
-        //File dir = new File("C:\\Users\\mayll\\OneDrive\\Documentos\\Lab1_OAC\\videoFiles\\frames\\S");
+        //Recebe o diretorio dos arquivos
+        File dir = new File("C:\\Users\\mayll\\OneDrive\\Documentos\\Lab1_OAC\\videoFiles\\frames\\teste\\");
+        
         //Cria o novo file que ira receber os dados
-        //File newFile = new File("C:\\Users\\mayll\\OneDrive\\Documentos\\Lab1_OAC\\videoFiles\\frames\\S\\bigS.s");
-        //toFileS toS = new toFileS(dir,newFile);
-        
-        
-        
-//    ProcessBuilder processBuilder = new ProcessBuilder("C:/Windows/System32/cmd.exe");
-//    processBuilder.directory(new File("C:\\Users\\mateu\\OneDrive\\Documentos\\OAC\\Lab 1\\Video Files\\frames"));
-
-    // -- Linux --
-
-    // Run a shell command
-    //processBuilder.command("bash", "-c", "ls /home/mkyong/");
-
-    // Run a shell script
-    //processBuilder.command("path/to/hello.sh");
-
-    // -- Windows --
-
-    // Run a command
-    
-//    Process process = processBuilder.start();
-//    BufferedWriter p_stdin = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
-    File dir = new File("C:\\Users\\mateu\\OneDrive\\Documentos\\OAC\\Lab 1\\Video Files\\frames");
-    for (File file : dir.listFiles()) {
-
-        if(file.getName().contains(".bmp")) {
-            String fileName = file.getName().replace(".bmp", "");
-            System.out.println(fileName);
-            String result = executeCommand("cd C:\\Users\\mateu\\OneDrive\\Documentos\\OAC\\Lab 1\\Video Files\\frames && bmp2oac3.exe "+ fileName + ".bmp");
-            executeCommand("cd C:\\Users\\mateu\\OneDrive\\Documentos\\OAC\\Lab 1\\Video Files\\frames && del /f " + fileName + ".mif");
-            executeCommand("cd C:\\Users\\mateu\\OneDrive\\Documentos\\OAC\\Lab 1\\Video Files\\frames && del /f " + fileName + ".data");
-            System.out.println(result);
-            
-            
-//            try {
-//                //single execution
-//                p_stdin.write("cd C:\\Users\\mayll\\OneDrive\\Documentos\\Lab1_OAC\\videoFiles\\frames\\teste");
-//                p_stdin.newLine();
-//                p_stdin.write("bmp2oac3.exe " + fileName + ".bmp");
-//                p_stdin.newLine();
-//                p_stdin.write("del /f " + fileName + ".mif");
-//                p_stdin.newLine();
-//                p_stdin.flush();
-//            }
-//            catch (IOException e) {
-//                System.out.println(e);
-//            }
-        
-        }
-        
-        
-        
-        //processBuilder.command("cmd.exe", "/c", "cd C:\\Users\\mayll\\OneDrive\\Documentos\\Lab1_OAC\\videoFiles\\frames\\imagens && bmp2oac3.exe " + fileName);
-    }
-//    Scanner s = new Scanner( process.getInputStream() );
-//    while (s.hasNext())
-//    {
-//        System.out.println( s.next() );
-//    }
-//       s.close();
-//    p_stdin.write("exit");
-//    p_stdin.newLine();
-//    p_stdin.flush();
-//    int exit = process.waitFor();
-//    System.out.println("Exit status: " + exit);
-
-        
-
-        
-        
-    }
-    
+        File newFile = new File("C:\\Users\\mayll\\OneDrive\\Documentos\\Lab1_OAC\\videoFiles\\frames\\teste\\teste1.bin");
+        /*toFileS toS = new toFileS(dir,newFile);
+        toS.writeFile();*/
+        toFileBin toB = new toFileBin(dir ,newFile);
+        toB.writeBin();          
+    }    
 }
--- src/readfiles/toFileBin.java
+++ src/readfiles/toFileBin.java
@@ -1,10 +1,45 @@
 package readfiles;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 
 public class toFileBin {
-    //private File dir;
-    //private File newFile;
-    //private FileWriter 
+    private OutputStream binNewFile;
+    private File dir;
+    private String fileName;
+    
+    public toFileBin(File dir, File newFile) throws java.io.FileNotFoundException{
+        //recebe como argumento a localização do arquivo que gostariamos de ler escrever
+        this.dir = dir;
+        //this.binDir = new FileInputStream(dir);
+        this.binNewFile = new FileOutputStream(newFile);
+        this.fileName = newFile.getName();
+    }
+    
+    public void writeBin() throws FileNotFoundException, IOException{
+        for(File file : this.dir.listFiles()){
+            System.out.println("loop for");
+            if(file.getName().contains(".bin") && !file.getName().equals(this.fileName)){
+                InputStream is = new FileInputStream(file);
+                int lerbyte = is.read();
+                while(lerbyte != -1){
+                    lerbyte = is.read();
+
+                    this.binNewFile.write(lerbyte);
+                    //System.out.println(file.getName());
+                } 
+                System.out.println("Arquivo " + file.getName() + " gerado" );
+                is.close();
+            }              
+        }
+        this.binNewFile.close();
+    }   
 }
+
+
